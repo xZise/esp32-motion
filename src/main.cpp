e490ded5ade134d398686ec9d3df73ecb045175a
Fabian Neundorf <CommodoreFabianus@gmx.de>
2024-05-17 11:41:00 +0200

    Separate function to query the shelly

--- src/main.cpp
+++ src/main.cpp
@@ -51,47 +51,59 @@ enum class SwitchResult {
   WasOff
 };
 
-SwitchResult setSwitch(uint8_t switchId, bool on) {
-  SwitchResult result = SwitchResult::Error;
+bool queryShelly(JsonDocument& doc, const char endpoint[]) {
   // wait for WiFi connection
-  if ((wifiMulti.run() == WL_CONNECTED)) {
-    HTTPClient http;
-
-    char url[100];
-    sprintf(url, "http://%s/rpc/Switch.Set?id=%d&on=%s", hostname, switchId, on ? "true" : "false");
-
-    Serial.print("[HTTP] begin...\n");
-    // configure traged server and url
-    http.begin(url);
-
-    Serial.print("[HTTP] GET...\n");
-    // start connection and send HTTP header
-    int httpCode = http.GET();
-
-    // httpCode will be negative on error
-    if (httpCode > 0) {
-      // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-      // file found at server
-      if (httpCode == HTTP_CODE_OK) {
-        String payload = http.getString();
-        Serial.println(payload);
-
-        JsonDocument doc;
-        deserializeJson(doc, payload);
-        bool was_on = doc["was_on"];
-
-        Serial.printf("Was on: %s\n", was_on ? "on" : "off");
-        result = was_on ? SwitchResult::WasOn : SwitchResult::WasOff;
-      }
-    } else {
-      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
-
-    http.end();
+  if ((wifiMulti.run() != WL_CONNECTED)) {
+    return false;
   }
-  return result;
+
+  HTTPClient http;
+
+  char url[100];
+  sprintf(url, "http://%s/%s", hostname, endpoint);
+
+  Serial.print("[HTTP] begin...\n");
+  // configure traged server and url
+  http.begin(url);
+
+  Serial.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if (httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+
+      deserializeJson(doc, payload);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+
+  return httpCode == HTTP_CODE_OK;
+}
+
+SwitchResult setSwitch(uint8_t switchId, bool on) {
+  char endpoint[100];
+  sprintf(endpoint, "rpc/Switch.Set?id=%d&on=%s", switchId, on ? "true" : "false");
+
+  JsonDocument doc;
+  if (queryShelly(doc, endpoint)) {
+    bool was_on = doc["was_on"];
+
+    Serial.printf("Was on: %s\n", was_on ? "on" : "off");
+    return was_on ? SwitchResult::WasOn : SwitchResult::WasOff;
+  } else {
+    return SwitchResult::Error;
+  }
 }
 
 void loop() {
